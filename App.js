325c5b092628aeb517c1547681da5d600152c612
esteban <esteban>
2019-10-20 19:44:42 +0200

    changes

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import UserNavigation from "./.expo/app/navigations/User";
+import firebaseConfig from "./.expo/app/utils/fireBase";
+import *  as firebase from "firebase";
+
+
+//configuracion de bd de firebase
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <UserNavigation />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
